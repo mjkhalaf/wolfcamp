79daf9140d8492c0b396ddeb9617717dbb2bc2e4
mjkhalaf <mohammed.j.alkhalaf-1@ou.edu>
2019-10-23 17:17:26 -0500

    Sketched a new class called student (with rating)

--- Desktop/Java 2 Porjects/Mohammed/src/Student.java
+++ Desktop/Java 2 Porjects/Mohammed/src/Student.java
@@ -1,4 +1,29 @@
 
-public class Student {
+public class Student extends Person implements Comparable<Student> {
+	private String className;
+	private Integer rating;
+	private static final int INTITAL_RATING = 5;
+	
+	public Student() throws NameException {
+		super();
+		rating = INTITAL_RATING;
+	}
+	
+	public Student(String firstName, String lastName, Integer rating) throws NameException {
+		// TODO Auto-generated constructor stub
+		super(firstName, lastName);
+		this.rating = rating;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		return rating - o.getRating();
+		
+	}
+
+	public Integer getRating() {
+		// TODO Auto-generated method stub
+		return new Integer(rating);
+	}
 
 }
